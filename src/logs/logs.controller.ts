c29b2a3deb2bdb81978810d4ab8adee2d7013f66
Eliton <eliton.pedrotti@ntconsult.com.br>
2025-04-03 16:37:38 -0300

    add swagger

--- src/logs/logs.controller.ts
+++ src/logs/logs.controller.ts
@@ -1,13 +1,34 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { LogsService } from './logs.service';
+import { ApiBody, ApiConsumes, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Express } from 'express';
 
+@ApiTags('Logs')
 @Controller('logs')
 export class LogsController {
   constructor(private readonly logsService: LogsService) {}
 
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
+  @ApiOperation({ summary: 'Upload de arquivo de log de partidas' })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
   async uploadLog(@UploadedFile() file: Express.Multer.File) {
     return this.logsService.processLogFile(file);
   }
